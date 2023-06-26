1c3e98afc115ffa8a45f605a1bd943bcf9562125
MilenaVergataRSE <milena.vergata@rse-web.it>
2023-06-26 09:15:51 +0200

    aggiunta_pagina_tools_aggiuntivi
    Collegamento a pagina html

--- lib/Views/AnalisiAvanzate.jsx
+++ lib/Views/AnalisiAvanzate.jsx
@@ -32,13 +32,50 @@ function AnalisiAvanzate(props) {
       {/* titolo della pagina */}
       <div className={classNames(PanelStyles.header)}>
         <label className={PanelStyles.heading}>
-          <h1> Naviga tra i tools di approfondimento </h1>
+          <h1> Navigate among the supplementary tools</h1>
         </label>
       </div>
 
       {/* caso multi energy*/}
       {/* collegamento a WEN*/}
       {/* collegamento a mapstore */}
+      <div className={classNames(PanelStyles.section, Styles.section)}>
+        <ul class="list-group list-group-flush">
+          <table>
+            <tr>
+              <td colspan="2">
+                {/* <h2> MODULO DI CALCOLO </h2> */}
+                <h2>TITLE</h2>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <a target="_blank" href="/marinewind_additional_tools.html">
+                  <img
+                    className={Styles.image}
+                    src={require("../../wwwroot/images/marinewind.png")}
+                    alt="Image not found"
+                  />
+                </a>
+              </td>
+              <td>
+                <p align="justify">
+                  Insert text and{" "}
+                  <a
+                    target="_blank"
+                    link
+                    className={Styles.link}
+                    href="/marinewind_additional_tools.html"
+                  >
+                    {" "}
+                    LINK
+                  </a>
+                </p>
+              </td>
+            </tr>
+          </table>
+        </ul>
+      </div>
     </MenuPanel>
   );
 }
